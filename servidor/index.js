2c42aacdc5e5cb5d819eb935f69754fead0af7ef
fdrsvf <>
2023-08-25 17:22:22 -0400

    git

--- servidor/index.js
+++ servidor/index.js
@@ -12,8 +12,6 @@ const express = require('express');
 const { usuario } = require('./models');
 
 
-
-
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -30,7 +28,7 @@ app.use(
     secret: process.env.SECRET,
     algorithms: ["HS256"],
     getToken: req => req.cookies.token
-  }).unless({ path: ["/autenticar", "/logar", "/deslogar", "/usuarios/cadastrar"] })
+  }).unless({ path: ["/autenticar", "/logar", "/deslogar", "/usuario/cadastrar"] })
 );
 
 app.get('/usuario/cadastrar', async function(req, res){
@@ -43,7 +41,7 @@ app.get('/usuario/listar', async function(req, res){
 
 app.post('/usuario/cadastrar', async function(req, res){
 
-if (req.body.senha === req.body.confirmeS)
+if (req.body.password === req.body.confirmeS)
       res.json({mensagem:"conseguiu"})
     else(
       res.json({mensagem:"vc não conseguiu"})
@@ -56,13 +54,12 @@ app.get('/autenticar', async function(req, res){
 app.get('/', async function(req, res){
   res.render("home")
 })
-
-
+  
 app.post('/logar', (req, res) => {
   let usuario = req.body.usuario
   let senha = req.body.senha
 
-  if(usuario === "natasha@logar" && senha === "2006" ){
+  if(usuario == "natasha@logar.com" && senha == "2006" ){
    const id = 1;
    const token = jwt.sign({id}, process.env.SECRET, {
     expiresIn:300
@@ -74,15 +71,36 @@ app.post('/logar', (req, res) => {
    })
 
   }
-  res.status(500).json({mensagem: " voê não foi logado"})
+  res.status(500).json({mensagem: " você não foi logado"})
 })
 
 app.post('/deslogar', function(req, res) {
   res.cookie('logar', null, {httpOnly: true})
   res.json({deslogado:true})
 })
+app.post('/usuario/cadastrar', async function(req, res){
+ if(req.body.senha == req.body.confirme){
+  await usuario.create(req.body)
+  res.redirect('usuario/listar')
+  req.json("cadastro feito com sucesso")
+ }else{
+  res.status(500).json("senha incorreta") 
+}})
+
+app.get('/', async function(req,res){
+  try{
+    var usuarios = await usuario.findAll();
+    req.render('home', {usuarios});
+
+  }catch(err){
+    console.log(err);
+    res.status(500).json({mensagem: 'ocorreu um erro ao buscar os usuarios'})
+  }
+})
+
 
 
 app.listen(3000, function() {
   console.log('App de Exemplo escutando na porta 3000!')
 });
+
