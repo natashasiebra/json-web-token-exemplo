a9938313b33150ac9998c700d1aa4c41422fb647
natashasiebra <natashasiebra3@gmail.com>
2023-11-17 15:45:41 -0400

    comit

---
+++ cliente/public/src/app/functions/validateToken.js
@@ -0,0 +1,16 @@
+import { jwtVerify } from "jose";
+
+const validateToken = async (token)=>{
+    const secret = "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY5NTY5MTU0MiwiaWF0IjoxNjk1NjkxNTQyfQ.EG-A9n08TjSSm2affCX5Bl3yRpDbIytMIO5j2iMalLA"
+    
+    try{
+     const isTokenValidate = await jwtVerify(token, 
+        new TextEncoder().encode(secret))
+        console.log(isTokenValidate)
+     if(isTokenValidate){
+        return  true
+    }
+}catch{ return false
+    }
+}   
+export {validateToken};
---
+++ cliente/public/src/app/layout.js
@@ -0,0 +1,18 @@
+
+export const metadata = {
+  title: 'Vingadores',
+  description: 'Project for class PTAC',
+}
+
+export default function RootLayout({ children }) {
+
+  return (
+
+    <html lang="pt-br">
+      <body>{children}
+      </body>
+ 
+    </html>
+
+  )
+}
